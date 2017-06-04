40a6cc36fb65a28a9dc7d8c34d44952175552d03
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-06-04 21:53:16 +0200

    ...

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ import psycopg2
 pprint.pprint(dict(os.environ), width=1)
 
 
-with psycopg2.connect(user=os.environ['POSTGRES_USER'], database=os.environ['POSTGRES_DB']) as conn:
+with psycopg2.connect(host='postgres', user=os.environ['POSTGRES_USER'], database=os.environ['POSTGRES_DB']) as conn:
     cr = conn.cursor()
