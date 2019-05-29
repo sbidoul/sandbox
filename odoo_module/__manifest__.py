32afef8bb33114c5e27b8933ac8db7760f377b51
fkantelberg <florian.kantelberg@initos.com>
2019-05-29 15:15:06 +0200

    Sumary

--- odoo_module/__manifest__.py
+++ odoo_module/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "odoo module",
     "version": "12.0.1.1.1",
     "author": "Author, Odoo Community Association (OCA)",
-    "summary": "summary",
+    "summary": "Just extending the summary",
     "license": "LGPL-3",
     "installable": True,
     "auto_install": False,
