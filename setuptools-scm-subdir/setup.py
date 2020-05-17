2dc2c3d19278fb93ebf9efbad28d4346d5e67a3d
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-05-17 14:16:26 +0200

    setuptools-scm-subdir

--- setuptools-scm-subdir/setup.py
+++ setuptools-scm-subdir/setup.py
@@ -4,5 +4,8 @@ from setuptools import setup
 setup(
     name="setuptools-scm-sudir",
     setup_requires=["setuptools_scm"],
-    use_scm_version=True,
+    use_scm_version={
+        "root": "..",
+        "relative_to": __file__,
+    },
 )
