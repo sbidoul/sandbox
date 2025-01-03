3f9e72e302fbaaec0c58a4e111fa186432f3ee93
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2025-01-03 14:06:58 +0100

    Add test model

--- myaddon/__manifest__.py
+++ myaddon/__manifest__.py
@@ -2,7 +2,5 @@
     "name": "my addon",
     "version": "17.0.1.0.0",
     "depends": [
-        "mis_builder",
-        "mis_builder_budget",
     ]
 }
---
+++ myaddon/models/mymodel.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+
+class MyModel(models.Model):
+    _name = "myaddon.mymodel"
+    _description = "My Model"
+
+    thename = fields.Char(string="The Name", required=True)
